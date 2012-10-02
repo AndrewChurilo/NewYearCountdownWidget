edc99c24504a762d7d096cebbd450b8c69b03215
AndrewChurilo <ferrari24@ya.ru>
2012-10-02 20:56:15 +0300

    Simple widget countdown to the New Year with style support.
    Signed-off-by: AndrewChurilo <ferrari24@ya.ru>

---
+++ src/by/chaba/widget/newyear/WidgetProvider.java
@@ -0,0 +1,55 @@
+package by.chaba.widget.newyear;
+
+/*
+ * Copyright (C) 2012 The Android Open Source Project 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * This class is desinged to start WidgetUpdateService when user starts widget
+ * 
+ * @author Andrei Churila
+ *
+ */
+public class WidgetProvider extends AppWidgetProvider {    
+    private Intent mServiceIntent;
+    
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+    
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+            int[] appWidgetIds) {        
+        mServiceIntent = new Intent(context, WidgetUpdateService.class);
+        context.startService(mServiceIntent);      
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+    
+    @Override
+    public void onDisabled(Context context) {
+        context.stopService(mServiceIntent);
+        super.onDisabled(context);
+    }
+}
